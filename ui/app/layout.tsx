b174dc009eb5dcda675cd6f0bbff8582129c024b
Bruno Menezes <brunodmenezes@gmail.com>
2025-02-15 12:23:26 +1300

    refactor: Update font configuration to avoid flickering.

--- ui/app/layout.tsx
+++ ui/app/layout.tsx
@@ -1,15 +1,22 @@
 import Header from "@/components/Header";
-import { gamjaFlowerRegular } from "@/styles/fonts";
 import type { Metadata } from "next";
+import { Gamja_Flower } from "next/font/google";
 
 import { Providers } from "@/providers/Providers";
 import "@rainbow-me/rainbowkit/styles.css";
 import "./globals.css";
 
 export const metadata: Metadata = {
-    title: "Coprocessor Exp",
+    title: "Scribbl",
     description: "Unbiased art judge",
 };
+
+const gamjamRegular = Gamja_Flower({
+    weight: "400",
+    style: "normal",
+    preload: true,
+    subsets: ["latin"],
+});
 
 export default function RootLayout({
     children,
@@ -19,7 +26,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body
-                className={`${gamjaFlowerRegular.className} antialiased custom text-foreground bg-background  bg-gradient-to-br from-primary-100 to-secondary-500`}
+                className={`${gamjamRegular.className} antialiased custom text-foreground bg-background  bg-gradient-to-br from-primary-100 to-secondary-500`}
             >
                 <Providers>
                     <Header />
