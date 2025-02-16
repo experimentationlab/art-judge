81191b2bf21dae3fe3d13f326baae0684c63d8a3
Bruno Menezes <brunodmenezes@gmail.com>
2025-02-16 22:14:32 +1300

    refactor(ui): RefetchOnBlockChange now can be enable/disable and invalidate queries based on number of blocks passed.

--- ui/hooks/useRefetchOnBlockChange.tsx
+++ ui/hooks/useRefetchOnBlockChange.tsx
@@ -4,11 +4,30 @@ import { useQueryClient } from "@tanstack/react-query";
 import { useEffect } from "react";
 import { useBlockNumber } from "wagmi";
 
-export const useRefetchOnBlockChange = (queryKey: readonly unknown[]) => {
+interface Opts {
+    /**
+     * Enables/disables listening for block number changes.
+     * @default true
+     */
+    watch?: boolean;
+    /**
+     * Number of blocks until it runs the query invalidations.
+     * @default 10n
+     */
+    numberOfBlocks?: bigint;
+}
+
+type Hook = (queryKey: readonly unknown[], opts?: Opts) => void;
+
+export const useRefetchOnBlockChange: Hook = (queryKey: readonly unknown[], opts) => {
+    const { watch = true, numberOfBlocks = 10n } = opts ?? {};
     const queryClient = useQueryClient();
-    const { data: blockNumber } = useBlockNumber({ watch: true });
+    const { data: blockNumber } = useBlockNumber({ watch });
 
     useEffect(() => {
-        queryClient.invalidateQueries({ queryKey });
-    }, [blockNumber, queryClient, queryKey]);
+        if (watch && blockNumber && blockNumber % numberOfBlocks === 0n) {
+            console.info(`${numberOfBlocks} blocks had passed. Refetching...`);
+            queryClient.invalidateQueries({ queryKey });
+        }
+    }, [blockNumber, queryClient, queryKey, watch]);
 };
