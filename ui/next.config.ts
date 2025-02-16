5b069dd4c2f074bc5537039989bcf401fecd14a9
Bruno Menezes <brunodmenezes@gmail.com>
2025-02-16 22:50:42 +1300

    feat: Add CSP declaration to the next config file.

--- ui/next.config.ts
+++ ui/next.config.ts
@@ -1,7 +1,36 @@
 import type { NextConfig } from "next";
 
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';  
+  font-src 'self';
+  frame-ancestors 'self' https://app.safe.global;
+`;
+
 const nextConfig: NextConfig = {
-  /* config options here */
+    reactStrictMode: true,
+    async headers() {
+        return [
+            {
+                source: "/((?!api|_next/static|_next/image|favicon.ico).*)",
+                headers: [
+                    {
+                        key: "Content-Security-Policy",
+                        value: `frame-ancestors 'self' https://app.safe.global;`,
+                    },
+                ],
+            },
+            {
+                source: "/manifest.json",
+                headers: [
+                    {
+                        key: "Content-Security-Policy",
+                        value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
